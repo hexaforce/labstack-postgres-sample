80340607d235b93038b39060403182eb90b07802
tantaka <tomokazu.tantaka@glpgs.com>
2022-12-21 09:39:05 +0900

    update

--- app/db/models.go
+++ app/db/models.go
@@ -1,1 +1,13 @@
 package db
+
+type Comment struct {
+	ID      int64  `json:"id"`
+	Comment string `json:"comment"`
+	UserID  int64  `json:"user_id"`
+	User    *User  `pg:"rel:has-one" json:"user"`
+}
+
+type User struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name"`
+}
